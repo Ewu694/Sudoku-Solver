6619d4e5ad8666826ae54119cea4ea0497815b3a
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-05-02 20:09:25 -0400

    Update SudokuSolver.hpp

--- SudokuSolver.hpp
+++ SudokuSolver.hpp
@@ -1,4 +1,4 @@
-/* 
+/*
     Name: Eric Wu
     Professor: Ms.Tiziana Ligorio
     Course: CS235
@@ -13,11 +13,11 @@
 #include <fstream>
 #include <sstream>
 
-//We define Location within the Sudoku namespace.
-//To use Location, you must specify the namespace Sudoku::Location
-//Just like you do when using the standard namespace std::
+// We define Location within the Sudoku namespace.
+// To use Location, you must specify the namespace Sudoku::Location
+// Just like you do when using the standard namespace std::
 namespace Sudoku
-{ 
+{
   struct Location
   {
     int row;
@@ -28,48 +28,52 @@ namespace Sudoku
 class SudokuSolver
 {
 public:
- 
-//Add your public members here
-SudokuSolver();//default constructors
-SudokuSolver(std::string input_file);//parameterized constructor, *** will need to implement other functions to support this and make it easier***
+  // Add your public members here
+  SudokuSolver();                       // default constructors
+  SudokuSolver(std::string input_file); // parameterized constructor, *** will need to implement other functions to support this and make it easier***
 
-bool isPuzzleSolvable();//isPuzzleSolvable() returns true if the puzzle is solvable, false otherwise. We suggest you keep track of this when solving the problem at construction, rather than solving the puzzle again each time this function is called.
-void setSolvable();//records if puzzle is solvable or not
-int** getPuzzleNumbers();//returns the 2D pointer array representing the puzzle
-void setPuzzleNumbers(int** puzzle);//sets the 2D pointer array representing the puzzle to the value of the parameter 
+  bool isPuzzleSolvable();             // isPuzzleSolvable() returns true if the puzzle is solvable, false otherwise. We suggest you keep track of this when solving the problem at construction, rather than solving the puzzle again each time this function is called.
+  void setSolvable();                  // records if puzzle is solvable or not
+  int **getPuzzleNumbers();            // returns the 2D pointer array representing the puzzle
+  void setPuzzleNumbers(int **puzzle); // sets the 2D pointer array representing the puzzle to the value of the parameter
+
+  /*
+  returnNextEmpty() returns a Location object with the coordinates of the first 0 found in the 2D pointer array, meaning that it is an empty square in the sudoku board.
+  To find the first empty square you must read the 2D array from top-left (0,0) to bottom right (8,8), one row at a time.
+  Set the row and col of the Location to the indices at which the first 0 is found in the 2D array.
+  If there are no empty squares the Location object should have row and col values of -1.
+  */
+  Sudoku::Location returnNextEmpty() const;
+
+  /*
+  checkLegalValue(int value, Sudoku::Location location) returns true if the value parameter (which can take on values 1-9) can legally be placed at the given location in the 2D array without breaking the rules of sudoku.
+  It returns false if placing value at location is not a legal move.
+  Remember, in order to be a valid move, the value must be unique with a row, column, and sub-grid.
+  */
+  bool checkLegalValue(int value, Sudoku::Location location);
+
+  /*
+  display will print the current board
+  -N is an integer 1-9 (the function displays the actual integer not an N)
+  -X represents a blank box where there is a zero (the function displays the X not the zero)
+  -After every 3 numbers (columns), we have a " | " to separate sub-grids. Note that each " | " is preceded by two space characters and followed by a single space character.
+  -After every 3 rows, we have 12 dashes " - " to separate the sub-grids.
+  */
+  void display();
 
 /*
-returnNextEmpty() returns a Location object with the coordinates of the first 0 found in the 2D pointer array, meaning that it is an empty square in the sudoku board. 
-To find the first empty square you must read the 2D array from top-left (0,0) to bottom right (8,8), one row at a time. 
-Set the row and col of the Location to the indices at which the first 0 is found in the 2D array. 
-If there are no empty squares the Location object should have row and col values of -1.
+Helper function for the void function to back track through the entire board and get the right values
 */
-Sudoku::Location returnNextEmpty() const;
+  bool SudokuSolution(Sudoku::Location location);
 
 /*
-checkLegalValue(int value, Sudoku::Location location) returns true if the value parameter (which can take on values 1-9) can legally be placed at the given location in the 2D array without breaking the rules of sudoku. 
-It returns false if placing value at location is not a legal move. 
-Remember, in order to be a valid move, the value must be unique with a row, column, and sub-grid.
+  Function to solve the sudoku board
 */
-bool checkLegalValue(int value, Sudoku::Location location);
-
-/*
-display will print the current board
--N is an integer 1-9 (the function displays the actual integer not an N)
--X represents a blank box where there is a zero (the function displays the X not the zero)
--After every 3 numbers (columns), we have a " | " to separate sub-grids. Note that each " | " is preceded by two space characters and followed by a single space character.
--After every 3 rows, we have 12 dashes " - " to separate the sub-grids.
-*/
-void display();
-
-/*
-Function to solve the sudoku board
-*/
-bool SudokuSolution();
+  void SudokuSolution();
 
 private:
-//Add your private members here
-bool puzzle_solvability_;
-int** puzzle_numbers_;
+  // Add your private members here
+  bool puzzle_solvability_;//indicates whether or not current puzzle is able to solved or not
+  int **puzzle_numbers_;//initalizing the 2D array that'll hold the values of the suduku board
 };
 #endif
